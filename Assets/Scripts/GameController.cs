25029de0455b2930c2d3ad2fe673afaa831a83dc
DavidEsdrs <carpina1983@gmail.com>
2023-08-21 00:52:30 -0300

    feat: Add logger - draw ui only at start

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -3,18 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameController : MonoBehaviour, IUICaller {
+  [SerializeField]
+  private Logger _logger;
+
   public CalendarController calendarController;
 
   public MatchController MatchController;
 
   public Club playerClub;
 
-  private CalendarUI dashboardUI;
+  private CalendarUI calendarUI;
 
   void Awake() {
     DontDestroyOnLoad(gameObject);
 
-    dashboardUI = GameObject.Find("Context").GetComponent<CalendarUI>();
+    calendarUI = calendarUI = GameObject.Find("Context").GetComponent<CalendarUI>();;
+
     // After we'll get the game info from a saving file
     calendarController = GameObject
       .FindGameObjectWithTag("CalendarController")
@@ -34,10 +38,13 @@ public class GameController : MonoBehaviour, IUICaller {
     );
 
     playerClub = sport;
+  }
+
+  void Start() {
     DrawUI();
   }
 
   public void DrawUI() {
-    dashboardUI.SetClub(playerClub.Name, Color.red);
+    calendarUI.SetClub(playerClub.Name, Color.red);
   }
 }
