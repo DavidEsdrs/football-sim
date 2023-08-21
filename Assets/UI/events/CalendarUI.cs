ffd0e0e26d1f63c868b9e61604e0eb0ed4c219cd
DavidEsdrs <carpina1983@gmail.com>
2023-08-21 01:28:43 -0300

    feat: Draw UI elements

--- Assets/UI/events/CalendarUI.cs
+++ Assets/UI/events/CalendarUI.cs
@@ -1,20 +1,24 @@
 using UnityEngine;
 using UnityEngine.UIElements;
 using System.Collections.Generic;
+using System.Linq;
 
 public class CalendarUI : MonoBehaviour
 {
+    [SerializeField]
+    private Logger _logger;
+
     [SerializeField]
     CalendarController calendarController;
 
     UIDocument document;
     Button nextBtn;
 
-    [SerializeField]
-    private string dateString;
+    private TextElement dateString;
 
-    [SerializeField]
-    private string club;
+    private Label club;
+
+    private ListView schedule;    
 
     [SerializeField]
     GameObject Calendar;
@@ -25,18 +29,33 @@ public class CalendarUI : MonoBehaviour
             .GetComponent<GameController>().calendarController;
 
         document = GetComponent<UIDocument>();
+
         nextBtn = document.rootVisualElement.Q<Button>("next-date");
-        nextBtn.clicked += calendarController.NextDay;
+        nextBtn.clicked += NextDay;
+
+        dateString = document.rootVisualElement.Q<TextElement>("date");
+
+        club = document.rootVisualElement.Q<Label>("club");
+
+        schedule = document.rootVisualElement.Q<ListView>("schedule");
+    }
+
+    private void NextDay() {
+        calendarController.NextDay();
     }
 
     public void SetDateString(string date) {
-        dateString = date;
+        dateString.text = date;
     }
 
     public void SetClub(string name, Color color) {
-        club = name;
+        club.text = name;
+        club.style.color = color;
     }
 
     public void SetEvents(List<IEventListener> events) {
+        _logger.Log("drawing events");
+        List<string> names = events.Select(ev => ev.Title).ToList();
+        schedule.itemsSource = names;
     }
 }
