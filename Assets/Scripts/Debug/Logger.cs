7d8e772d09fb6dc568a837c4692f33bf3096271c
DavidEsdrs <carpina1983@gmail.com>
2023-08-23 23:33:26 -0300

    feat: Create LogError and Error functions

--- Assets/Scripts/Debug/Logger.cs
+++ Assets/Scripts/Debug/Logger.cs
@@ -2,10 +2,24 @@ using UnityEngine;
 
 public class Logger : MonoBehaviour {
   public bool CanLog;
+  public bool ErrorLog = true;
 
   public void Log(object message) {
     if(CanLog) {
       Debug.Log(message);
     }
+  }
+
+  public void LogError(object message) {
+    if(ErrorLog) {
+      Debug.Log(message);
+    }
+  }
+
+  public System.Exception Error(string message) {
+    if(ErrorLog) {
+      Debug.Log(message);
+    }
+    return new System.Exception(message);
   }
 }
